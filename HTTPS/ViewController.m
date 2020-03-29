ebf38adf5180c07b4f1d95ceba785425d3ce4cbe
xiaotuan <734871221@qq.com>
2020-03-29 10:23:46 +0800

    1

--- HTTPS/ViewController.m
+++ HTTPS/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<NSURLSessionDelegate>
 
 @end
 
@@ -18,8 +18,57 @@
 {
     [super viewDidLoad];
 
+    NSURLSessionConfiguration * config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession * urlSesseion = [NSURLSession sessionWithConfiguration:config delegate:self  delegateQueue:nil];
+    
+    urlSesseion = [NSURLSession sharedSession];
+    
+    NSURLRequest * request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"] cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:100];
+    NSURLSessionDataTask * task =  [urlSesseion dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSLog(@"%@",[[NSString alloc]initWithData:data encoding:NSUTF8StringEncoding]);
+        
+    }];
+    
+    [task resume];
 }
 
+- (void)URLSession:(NSURLSession *)session didBecomeInvalidWithError:(nullable NSError *)error
+{
+    
+    
+}
+
+- (void)URLSession:(NSURLSession *)session didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
+ completionHandler:(void (^)(NSURLSessionAuthChallengeDisposition disposition, NSURLCredential * _Nullable credential))completionHandler
+{
+    // 如果使用默认的处置方式，那么 credential 就会被忽略
+    NSURLSessionAuthChallengeDisposition disposition = NSURLSessionAuthChallengePerformDefaultHandling;
+    NSURLCredential *credential = nil;
+    
+    if ([challenge.protectionSpace.authenticationMethod
+         isEqualToString:
+         NSURLAuthenticationMethodServerTrust]) {
+        
+        /* 调用自定义的验证过程 */
+        if (YES)
+        {
+            credential = [NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust];
+            if (credential) {
+                disposition = NSURLSessionAuthChallengeUseCredential;
+            }
+        } else {
+            /* 无效的话，取消 */
+            disposition = NSURLSessionAuthChallengeCancelAuthenticationChallenge ;
+        }
+    }
+    if (completionHandler) {
+        completionHandler(disposition, credential);
+    }
+    
+
+}
 
 
 @end
